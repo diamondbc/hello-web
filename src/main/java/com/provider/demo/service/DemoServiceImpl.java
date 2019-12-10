2986ccaa61d902f22fff827b2d7714280693487e
binchuan.wbc <binchuan.wbc@gongdao.com>
2019-12-10 11:18:54 +0800

    init

--- src/main/java/com/provider/demo/service/DemoServiceImpl.java
+++ src/main/java/com/provider/demo/service/DemoServiceImpl.java
@@ -1,16 +1,12 @@
 package com.provider.demo.service;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.util.Properties;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DemoServiceImpl {
 
-    @Value("${test.info}")
+    @Value("${test.info:testInfo}")
     private String testInfo;
 
     public String hello(String name) {
@@ -19,13 +15,4 @@ public class DemoServiceImpl {
         return str;
     }
 
-    public void received(String data) {
-        Properties properties = new Properties();
-        try {
-            properties.load(new StringReader(data));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        testInfo = properties.getProperty("test.info");
-    }
 }
